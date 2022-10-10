447a1a55ef1e9558b0b6acfc3dc53975939e2e02
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-10-11 00:21:37 +0530

    Describe_Table

---
+++ Desc_Table/Desc_table.sql
@@ -0,0 +1,16 @@
+-- create a table
+CREATE TABLE employee (
+  id INTEGER ,
+  name VARCHAR(30) ,
+  gender VARCHAR(10)
+);
+
+
+-- Show Tables
+show TABLES;
+
+-- show columns 
+SHOW COLUMNS FROM employee;
+
+-- Describe table 
+DESC employee;
