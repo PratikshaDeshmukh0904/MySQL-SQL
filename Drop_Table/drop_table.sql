73dc4dd7b34782e769cc60fafef120709553b16b
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-10-12 00:07:53 +0530

    Drop_Table

---
+++ Drop_Table/drop_table.sql
@@ -0,0 +1,11 @@
+-- create table 
+CREATE TABLE employee (
+    employeeid int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+-- DROP TABLE statement is used to drop an existing table in a database.
+drop table employee;
